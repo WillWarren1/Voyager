c6dc7d6b4b68ee0fc49bc48211c1d0d98631c190
Will Warren <willwarrenmail@gmail.com>
2019-04-08 11:48:39 -0400

    updated controllers

---
+++ Controllers/PlayersController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Voyager.Models;
+using content;
+
+namespace content.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class PlayersController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public PlayersController()
+    {
+      this._context = new DatabaseContext();
+    }
+
+    // GET: api/Players
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Player>>> GetPlayers()
+    {
+      return await _context.Players.ToListAsync();
+    }
+
+    // GET: api/Players/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Player>> GetPlayer(int id)
+    {
+      var player = await _context.Players.FindAsync(id);
+
+      if (player == null)
+      {
+        return NotFound();
+      }
+
+      return player;
+    }
+
+    // PUT: api/Players/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPlayer(int id, Player player)
+    {
+      if (id != player.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(player).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!PlayerExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Players
+    [HttpPost]
+    public async Task<ActionResult<Player>> PostPlayer(Player player)
+    {
+      _context.Players.Add(player);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetPlayer", new { id = player.Id }, player);
+    }
+
+    // DELETE: api/Players/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Player>> DeletePlayer(int id)
+    {
+      var player = await _context.Players.FindAsync(id);
+      if (player == null)
+      {
+        return NotFound();
+      }
+
+      _context.Players.Remove(player);
+      await _context.SaveChangesAsync();
+
+      return player;
+    }
+
+    private bool PlayerExists(int id)
+    {
+      return _context.Players.Any(e => e.Id == id);
+    }
+  }
+}
---
+++ Controllers/TreasuresController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Voyager.Models;
+using content;
+
+namespace content.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TreasuresController : ControllerBase
+  {
+    private readonly DatabaseContext _context;
+
+    public TreasuresController()
+    {
+      this._context = new DatabaseContext();
+    }
+
+    // GET: api/Treasures
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Treasure>>> GetTreasures()
+    {
+      return await _context.Treasures.ToListAsync();
+    }
+
+    // GET: api/Treasures/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Treasure>> GetTreasure(int id)
+    {
+      var treasure = await _context.Treasures.FindAsync(id);
+
+      if (treasure == null)
+      {
+        return NotFound();
+      }
+
+      return treasure;
+    }
+
+    // PUT: api/Treasures/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTreasure(int id, Treasure treasure)
+    {
+      if (id != treasure.Id)
+      {
+        return BadRequest();
+      }
+
+      _context.Entry(treasure).State = EntityState.Modified;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!TreasureExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // POST: api/Treasures
+    [HttpPost]
+    public async Task<ActionResult<Treasure>> PostTreasure(Treasure treasure)
+    {
+      _context.Treasures.Add(treasure);
+      await _context.SaveChangesAsync();
+
+      return CreatedAtAction("GetTreasure", new { id = treasure.Id }, treasure);
+    }
+
+    // DELETE: api/Treasures/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Treasure>> DeleteTreasure(int id)
+    {
+      var treasure = await _context.Treasures.FindAsync(id);
+      if (treasure == null)
+      {
+        return NotFound();
+      }
+
+      _context.Treasures.Remove(treasure);
+      await _context.SaveChangesAsync();
+
+      return treasure;
+    }
+
+    private bool TreasureExists(int id)
+    {
+      return _context.Treasures.Any(e => e.Id == id);
+    }
+  }
+}
